f0e84f94c656d163ca0df90c096b835a1cfe6fbd
Stephen Brown <stephen.jr36@gmail.com>
2024-08-09 16:04:44 -0600

    cleaned up comments in further-study.js

--- src/further-study.js
+++ src/further-study.js
@@ -32,57 +32,6 @@
 //   buildWordChain(['zoo', 'sour', 'racket', 'octos']);
 //   => ['zoo', 'octos', 'sour', 'racket']
 
-
-
-// function buildWordChain(words) {
-//     // Add first word to arr and remove from words
-//     const arr = [words[0]];
-//     words.splice(0, 1);
-
-//     // Iterate over remainding words to see if first letter of word in words is equal to last letter of last word in arr
-//     while (words.length > 0) {
-//         for (const word of words) {
-//             if (word[0] === arr[arr.length - 1][arr[arr.length - 1].length - 1]) {
-//                 arr.push(word);
-//                 words.splice(words.indexOf(word), 1)
-                
-//             } else {
-//                 return arr;
-//             }
-//         }
-//     }
-//     return arr;
-// }
-
-
-
-// function buildWordChain(words) {
-//     // for every word in words
-//     for (let i = 0; i < words.length; i++) {
-//         // for every word following word[i] in words
-//         console.log(``);
-//         innerloop: for (let j = i + 1; j < words.length; j++) {
-//             console.log(`words[i]:`, words[i])
-//             console.log(`words[j]:`, words[j])
-//             if (words[i][words[i].length - 1] === words[j][0]) {
-//                 console.log(`last letter of words[i] is the same as first letter of words[j]`)
-//                 console.log(`words pre slice`, words)
-//                 let word = words.splice(j, 1)[0];
-//                 words.splice(i + 1, 0, word)
-//                 console.log(`words post slice;`, words);
-//                 break innerloop;
-//             } else {
-//                 console.log(`not the same`)
-//             }
-//             console.log(``)
-//         }
-//     }
-//     console.log(words);
-//     return words;
-// }
-
-
-
 function buildWordChain(words) {
     const arr = [words[0]];
     words.splice(0, 1);
@@ -94,28 +43,5 @@ function buildWordChain(words) {
     }
     return arr;
 }
-// function buildWordChain(words) {
-//     const arr = [words[0]];
-//     words.splice(0, 1);
-//     // console.log(words)
-//     for (let i = 0; i < words.length; i++) {
-//         // console.log()
-//         // console.log(`Iteration ${i + 1}`)
-//         // console.log(`word[i]:`, words[i])
-//         // console.log(`arr:`, arr)
-//         console.log(`Last letter of last word in arr:`, arr[arr.length - 1][arr[arr.length - 1].length - 1])
-//         if (words[i][0] === arr[arr.length - 1][arr[arr.length - 1].length - 1]) {
-//             arr.push(words.splice(i, 1)[0])
-//             console.log(`arr post push:`, arr)
-//             i = -1;
-//         } else {
-//             console.log(`First letter of word[i] is not equal to last letter of last word of arr`)
-//         }
-//         console.log(`i:`, i)
-//     }
-//     return arr;
-// }
-buildWordChain(['zoo', 'sour', 'racket', 'octos'])
-// console.log(buildWordChain(['zoo', 'sour', 'racket', 'octos']))
 
 export { buildWordChain };
